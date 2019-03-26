4a87feea096195578aec7fb9ad49292326ad37a6
Rain120 <chao1202016@outlook.com>
2019-03-26 21:29:53 +0800

    index init

--- src/components/MHeader/index.tsx
+++ src/components/MHeader/index.tsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import { Row, Col, AutoComplete  } from 'antd';
+import './index.scss'
 
 export default class Layout extends Component {
   render() {
     return (
       <div className='header-wrapper'>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/my'>My</NavLink>
+        <Row
+          type="flex"
+          align="middle">
+          <Col span={8}>
+            <div>网易云音乐</div>
+          </Col>
+          <Col span={8}>
+            <ul className='menus'>
+              <li>发现音乐</li>
+              <li>我的音乐</li>
+              <li>朋友</li>
+              <li>音乐人</li>
+              <li>下载客户端</li>
+            </ul>
+          </Col>
+          <Col span={8}>
+            <div className='search-box'>
+              <AutoComplete dataSource={[]} />
+            </div>
+            <div className='user'>user</div>
+          </Col>
+          <Col span={24}>
+            <div className='submenus'>123</div>
+          </Col>
+        </Row>
       </div>
     )
   }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Rainy
  * @Github: https://github.com/Rain120
  * @Date: 2019-03-21 21:23:13
- * @LastEditTime: 2019-03-21 21:47:26
+ * @LastEditTime: 2019-03-25 22:58:39
  */
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
@@ -17,6 +17,7 @@ export default class Routes extends Component {
       <Router>
         <div>
           <MHeader />
+          <div style={{ clear: 'both' }}/>
           <Switch>
             <Route exact={true} path="/" component={Home}/>
             <Route path="/my" component={Mine}/>
